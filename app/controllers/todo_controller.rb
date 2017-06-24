8c8236dcade215dc906ccc49aa7882e573df7c7f
Quinn Collins <greendabean@gmail.com>
2017-06-24 18:30:47 +0000

    Revised conditionals to add descriptions to pages

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,18 +4,22 @@ class TodoController < ApplicationController
     end
     
     def show
+        @todo_description = "Description of todo"
+        @todo_pomodoro = "Amount of pomodoros necessary for task"
         @id = params[:id]
         if @id == '1'
-                    @todo_description = "Description of todo"
-        @todo_pomodoro = "Amount of pomodoros necessary for task"
+            @todo_description = "Study for the upcoming geometry quiz."
        
         
         elsif @id == '2'
-             @todo_description = "the number 2"
-        @todo_pomodoro = "4"
-        else
-          @todo_description = "else happened"
-        @todo_pomodoro = "67"
+          @todo_description = "Feed your cat cause she's probably hungry."
+          
+        elsif @id =='3'
+        @todo_description = "You're tired, sleep."
+        elsif @id=='4'
+        @todo_description = "Do your gaming final project because processing is hard."
+        elsif @if=='5'
+        @todo_description = "Take out the trash it stinks."
         end
     end
 end
