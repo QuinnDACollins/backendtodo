b19d27bec7d8d4ca7bac067703441a3ed2acab30
Quinn Collins <greendabean@gmail.com>
2017-06-24 20:20:29 +0000

    Assigned data base to id and made a variable to do so, displayed parts of database in show page

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,22 +4,23 @@ class TodoController < ApplicationController
     end
     
     def show
-        @todo_description = "Description of todo"
-        @todo_pomodoro = "Amount of pomodoros necessary for task"
-        @id = params[:id]
-        if @id == '1'
-            @todo_description = "Study for the upcoming geometry quiz."
+        @todo = Todo.find_by_id(params[:id])
+      # @todo_description = "Description of todo"
+     #   @todo_pomodoro = "Amount of pomodoros necessary for task"
+     #   @id = params[:id]
+        # if @id == '1'
+        #     @todo_description = "Study for the upcoming geometry quiz."
        
         
-        elsif @id == '2'
-          @todo_description = "Feed your cat cause she's probably hungry."
+        # elsif @id == '2'
+        #   @todo_description = "Feed your cat cause she's probably hungry."
           
-        elsif @id =='3'
-        @todo_description = "You're tired, sleep."
-        elsif @id=='4'
-        @todo_description = "Do your gaming final project because processing is hard."
-        elsif @if=='5'
-        @todo_description = "Take out the trash it stinks."
-        end
+        # elsif @id =='3'
+        # @todo_description = "You're tired, sleep."
+        # elsif @id=='4'
+        # @todo_description = "Do your gaming final project because processing is hard."
+        # elsif @if=='5'
+        # @todo_description = "Take out the trash it stinks."
+     #   end
     end
 end
