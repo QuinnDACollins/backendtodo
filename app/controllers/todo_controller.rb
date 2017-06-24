b378ba1795017548a443500773fd67f7d0396cd9
Quinn Collins <greendabean@gmail.com>
2017-06-24 18:19:27 +0000

    Conditionals

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,8 +4,18 @@ class TodoController < ApplicationController
     end
     
     def show
-        @todo_description = "Description of todo"
-        @todo_pomodoro = "Amount of pomodoros necessary for task"
         @id = params[:id]
+        if @id == '1'
+                    @todo_description = "Description of todo"
+        @todo_pomodoro = "Amount of pomodoros necessary for task"
+       
+        
+        elsif @id == '2'
+             @todo_description = "the number 2"
+        @todo_pomodoro = "4"
+        else
+          @todo_description = "else happened"
+        @todo_pomodoro = "67"
+        end
     end
 end
