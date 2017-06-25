58d9af3407236c2d64417f4c5956df5cd4fd8249
Quinn Collins <greendabean@gmail.com>
2017-06-25 16:01:48 +0000

    Added update and destory routes, and actions

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -23,6 +23,33 @@ class TodoController < ApplicationController
         # @todo_description = "Take out the trash it stinks."
      #   end
     end
+    
     def new
+    end
+    
+    def create
+        t = Todo.new
+         t.description = params['description']
+         t.pomodoro_estimate = params['pomodoro_estimate']
+         t.save
+         redirect_to "/todo/show/#{ t.id }"
+    end
+    
+    def edit
+        @todo = Todo.find(params[:id])
+    end
+    
+    def update
+        t = Todo.find(params[:id])
+         t.description = params['description']
+         t.pomodoro_estimate = params['pomodoro_estimate']
+         t.save
+         redirect_to "/todo/show/#{ t.id }"
+    end 
+    
+    def destroy
+        t = Todo.find(params[:id])
+        t.destroy
+        redirect_to "/todo/index/"
     end
 end
